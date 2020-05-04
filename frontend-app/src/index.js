e8b22b3e6e1a32b519aaa0b9ab4d86a671df1273
yuya okada <yuya.okada527@gmail.com>
2020-05-04 18:24:42 +0900

    add fe base

--- frontend-app/src/index.js
+++ frontend-app/src/index.js
@@ -1,138 +1,91 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
+class TopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        user_id: "12345678",
+        user_name: "user",
+      },
+    };
   }
 
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <Header title="Murmur" />
+        <Murmurs />
+        <PostArea />
       </div>
     );
   }
 }
 
-class Game extends React.Component {
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  );
+};
+
+class Murmurs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [
+      murmurs: [
         {
-          squares: Array(9).fill(null),
+          id: 1,
+          user_id: "00000001",
+          user_name: "user1",
+          message: "message1",
+          time: "2020-04-23T10:20:30.400+02:30",
+        },
+        {
+          id: 2,
+          user_id: "00000002",
+          user_name: "user2",
+          message: "message2",
+          time: "2020-04-27T10:20:30.400+02:30",
         },
       ],
-      xIsNext: true,
-      stepNumber: 0,
     };
   }
 
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([{ squares: squares }]),
-      xIsNext: !this.state.xIsNext,
-      stepNumber: history.length,
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0,
-    });
-  }
-
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = "Go to " + move ? "move #" + move : "game start";
+    const murmurs = this.state.murmurs.map((murmur) => {
       return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
+        <div>
+          <li key={murmur.id}>
+            <Murmur value={murmur} />
+          </li>
+        </div>
       );
     });
-    let status;
-    if (winner) {
-      status = "winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
+    return <div>{murmurs}</div>;
+  }
+}
+
+const Murmur = (props) => {
+  return (
+    <div class="card">
+      <div class="user-info">
+        {props.value.user_name + "@" + props.value.user_id}
       </div>
-    );
-  }
-}
+      <div class="message-area">{props.value.message}</div>
+    </div>
+  );
+};
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+const PostArea = (props) => {
+  return (
+    <form>
+      <input></input>
+      <button />
+    </form>
+  );
+};
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<TopPage />, document.getElementById("root"));
